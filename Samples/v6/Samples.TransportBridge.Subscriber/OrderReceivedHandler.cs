029a587bc124b83a9d797cde595c4997142bfaa6
sfarmar <s.farmar@gmail.com>
2017-09-11 11:29:12 +0100

    Added a NServiceBus v6.x sample

---
+++ Samples/v6/Samples.TransportBridge.Subscriber/OrderReceivedHandler.cs
@@ -0,0 +1,19 @@
+namespace Samples.TransportBridge.Subscriber
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Logging;
+    using Shared;
+
+    public class OrderReceivedHandler :
+        IHandleMessages<OrderReceived>
+    {
+        static ILog log = LogManager.GetLogger<OrderReceivedHandler>();
+
+        public Task Handle(OrderReceived message, IMessageHandlerContext context)
+        {
+            log.Info($"Subscriber has received OrderReceived event with OrderId {message.OrderId}.");
+            return Task.CompletedTask;
+        }
+    }
+}
